83e3a5888a7ac4e11f0d7471a62439bf512f2f21
Neshodym <voinash@gmail.com>
2023-04-25 23:28:52 +0900

    1

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Section from "./components/Section/Section";
+import SocialProfile from "./components/SocialProfile/SocialProfile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import user from "./data/user.json";
+import uploadStats from "./data/dataStats.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+
+export default function App() {
+  return (
+    <div>
+      <Section title={"Profile"}>
+        <SocialProfile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section title={"Statistics"}>
+        <Statistics title="Upload stats" stats={uploadStats} />
+      </Section>
+      {/* <Section>
+        <Statistics stats={uploadStats} />
+      </Section> */}
+
+      <Section title={"Friend"}>
+        <FriendList friends={friends} />
+      </Section>
+
+      <Section title={"Transaction"}>
+        <TransactionHistory transactions={transactions} />
+      </Section>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+
